2746c2bb1ca6675a3c83e03b256fe03efeb70b50
ingvoo <ingvi101@gmail.com>
2024-08-14 16:40:12 +0200

    feat: setup lazy loading for organizations

--- src/app/components/ListContainer.tsx
+++ src/app/components/ListContainer.tsx
@@ -4,19 +4,45 @@ import React from "react";
 import ListView from "./ListView";
 import AppNotification from "./AppNotification";
 
+type IOrganization = {
+  id: number;
+  login: string;
+  description?: string;
+  url?: string;
+};
+
+const endPoint = "https://api.github.com/organizations?per_page=5";
+
 export default function ListingsContainerComponent() {
-  const [data, setData] = React.useState(null);
+  const [data, setData] = React.useState<Array<IOrganization> | null>(null);
   const [hasError, setHasError] = React.useState(false);
-  const endPoint = "https://api.github.com/organizations?per_page=5";
+
+  function loadMoreOrganizations() {
+    if (!data) {
+      return;
+    }
+
+    const id = data[data.length - 1].id;
+
+    fetch(`${endPoint}&since=${id}`)
+      .then((res) => {
+        if (res.status === 200) return res.json();
+        throw new Error("error");
+      })
+      .then((res) => {
+        setData([...data, ...res]);
+      })
+      .catch((err) => {
+        console.error("Error: ", err);
+        setHasError(true);
+      });
+  }
 
   React.useEffect(() => {
     fetch(endPoint)
       .then((res) => {
-        if (res.status === 200) {
-          return res.json();
-        } else {
-          throw new Error("error");
-        }
+        if (res.status === 200) return res.json();
+        throw new Error("error");
       })
       .then((res) => setData(res))
       .catch((err) => {
@@ -39,5 +65,14 @@ export default function ListingsContainerComponent() {
     return <AppNotification message="Did not find any Organisations" />;
   }
 
-  return <ListView listings={data} />;
+  return (
+    <>
+      <ListView listings={data} />
+      <footer className="mt-8 flex items-center justify-center">
+        <button onClick={loadMoreOrganizations} className="border px-6 py-2">
+          Load more
+        </button>
+      </footer>
+    </>
+  );
 }
